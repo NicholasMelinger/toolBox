718a7a29368b927cc3d6dccf999d561f425f5843
Nicholas.Melinger <nicholas.melinger@gmail.com>
2018-06-26 08:30:53 +0200

    e

--- scripts_php/get_arbo.php
+++ scripts_php/get_arbo.php
@@ -1,16 +1,66 @@
 <?php
 
-	$link = mysqli_connect("localhost","root","", "db_boiteaoutils");
+	/*if(isset($_POST['id_techno'])){
+		$link = mysqli_connect("localhost","root","", "db_boiteaoutils");
 
-	$continue = true;
-	$id_techno = $_POST['id_techno'];
+		$continue = true;
+		$id_techno = $_POST['id_techno'];
+
+		$query = "SELECT libelleTechno FROM technologie WHERE idTechnologie = " . $id_techno;
+		$res= mysqli_query($link, $query);
+		$libelle_techno_fils = mysqli_fetch_assoc($res)['libelleTechno'];
+
+
+		$query = "SELECT IDTechnoPere FROM matricetechnos WHERE IDTechnoFils = " . $id_techno;
+		$res= mysqli_query($link, $query);
+		$id_techno_pere = mysqli_fetch_assoc($res)['IDTechnoPere'];
+
+		if($id_techno_pere != '0'){
+			$query =  "SELECT DISTINCT IDTechnologie, libelleTechno, brancheFinale, descLibelle, URL_image, URL_doc
+			  			FROM technologie 
+			  			WHERE idTechnologie = " .$id_techno_pere;
+
+			//echo $query . "</br>";
+			$res= mysqli_query($link, $query);
+			$techno_pere = mysqli_fetch_assoc($res);
+
+			echo "<td><div class='col-sm-2 zoom tuile-arbo' id='" . $techno_pere['IDTechnologie'] . "' style='background-color: #F7CA18;' onclick='get_technos(" . $techno_pere['IDTechnologie'] . ");set_arbo(" . $techno_pere['IDTechnologie'] . ");'>";
+			echo 	$libelle_techno_fils; 
+			echo "</div></td>";
+			//echo $techno['libelleTechno'];
+		}
+		else{
+
+			echo "<td><div class='col-sm-2 zoom tuile-arbo' style='background-color: #F7CA18;'>";
+			echo "<a href='http://localhost/Boiteaoutils/ToolBoxV2/trouver_outil.html'>". $libelle_techno_fils ."</a>";
+			echo "</div></td>";
+			//echo "continue = false" . "</br>";
+
+			$continue = false;
+		}
+	}*/
+
+
+
+	//Ancienne version 
 
 	/*$query = "SELECT libelleTechno FROM technologie WHERE idTechnologie = " . $id_techno;
 	$res= mysqli_query($link, $query);
 	$libelle_techno_arbo = mysqli_fetch_assoc($res)['IDTechnoPere'];*/
 
+	$link = mysqli_connect("localhost","root","", "db_boiteaoutils");
+
+	$continue = true;
+	$id_techno = $_POST['id_techno'];
+	$i = 0;
+	$tableau_html = array();
+	$colors =  array('#1C1C1C','#0B3B2E','#3B0B0B','#0A0A2A','#61380B','#0B3861');
+
+
 	echo "<table><tr>";
 	while($continue){
+		$html = "";
+
 		$id_techno_save = $id_techno;
 		$query = "SELECT IDTechnoPere FROM matricetechnos WHERE IDTechnoFils = " . $id_techno;
 		//echo $query . "</br>";
@@ -18,7 +68,7 @@
 
 		$id_techno = mysqli_fetch_assoc($res)['IDTechnoPere'];
 
-		echo "techno pere : " . $id_techno . "</br>";;
+		//echo "techno pere : " . $id_techno . "</br>";;
 		if($id_techno != '0'){
 			$query =  "SELECT DISTINCT IDTechnologie, libelleTechno, brancheFinale, descLibelle, URL_image, URL_doc
 			  			FROM technologie 
@@ -33,15 +83,17 @@
 			  FROM technologie 
 			  WHERE idTechnologie IN 
 			  (SELECT IDTechnoFils FROM matricetechnos WHERE IDTechnoPere = " . $techno_pere['IDTechnologie'] . ")";
-			 echo $query . "</br>";
+			 //echo $query . "</br>";
 
 			$res= mysqli_query($link, $query);
 
 			$techno_fils = mysqli_fetch_assoc($res);
 
-			echo "<td><div class='col-sm-2 zoom tuile' id='" . $techno_pere['IDTechnologie'] . "' style='background-color: #F7CA18;' onclick='get_technos(" . $techno_pere['IDTechnologie'] . ");set_arbo(" . $techno_pere['IDTechnologie'] . ");'>";
-			echo 	$techno_fils['libelleTechno']; 
-			echo "</div></td>";
+			$html = $html .  "<td><div class='col-sm-2 zoom tuile-arbo' id='" . $techno_pere['IDTechnologie'] . "' style='background-color: " . $colors[rand(0, 5)] . ";' onclick='get_technos(" . $techno_pere['IDTechnologie'] . ");set_arbo(" . $techno_pere['IDTechnologie'] . ");'>";
+			$html = $html .  	$techno_fils['libelleTechno']; 
+			$html = $html .  "</div></td>";
+
+			$tableau_html[$i]=$html;
 			//echo $techno['libelleTechno'];
 		}
 		else{
@@ -53,11 +105,24 @@
 
 			$techno = mysqli_fetch_assoc($res);
 
-			//echo "continue = false" . "</br>";
-			echo "<a href='http://localhost/InterfaceWeb/ToolBox/toolBox/trouver_outil.html'>". $techno['libelleTechno'] ."</a>";
+			$html = $html . "<td><a href='http://localhost/Boiteaoutils/ToolBoxV2/trouver_outil.html' style='color:white;font-weight:bold;'><div class='col-sm-2 zoom tuile-arbo' style='background-color: " . $colors[rand(0, 5)] . ";'>";
+			$html = $html . $techno['libelleTechno'];
+			$html = $html . "</div></a></td>";
+
+			$tableau_html[$i]=$html;
+
 			$continue = false;
 		}
+		$i++;
 	}
+
+	$j = count($tableau_html)-1; 
+	//echo "J : " . $j;
+//var_dump($tableau_html);
+	for($j; $j>=0; $j--){
+		echo $tableau_html[$j];
+	}
+
 	echo "</tr></table>";
 
 ?>
--- scripts_php/get_technos.php
+++ scripts_php/get_technos.php
@@ -5,13 +5,18 @@
 	$link = mysqli_connect("localhost","root","", "db_boiteaoutils");
 
 
+
 	// récupération ID matrice selectionnée
 	$query_get_technos = "SELECT DISTINCT IDTechnologie, libelleTechno, brancheFinale, descLibelle, URL_image, URL_doc
 			  FROM technologie 
 			  WHERE idTechnologie IN 
 			  (SELECT IDTechnoFils FROM matricetechnos WHERE IDTechnoPere = " . $_POST['id_techno'] . ")";
+	$res_technos= mysqli_query($link, $query_get_technos);
 
-	$res_technos = mysqli_query($link, $query_get_technos);
+
+
+	$colors =  array('#1C1C1C','#0B3B2E','#3B0B0B','#0A0A2A','#61380B','#0B3861');
+
 
 	//echo $query_get_technos;
 	echo "<table><tr>";
@@ -24,16 +29,13 @@
 
 
 				if($row['brancheFinale'] == "1") { // Branche finale
-					echo "<td><a data-fancybox data-type='iframe' data-src='techno.php?id_techno=" . $row['IDTechnologie'] . "' href='javascript:;'>";
-						echo "<img src='" . $row['URL_image'] . "' class='col-sm-2 zoom tuile' id='" . $row['IDTechnologie'] . "'  >";
-						/*echo "<div class='col-sm-2 zoom tuile' id='" . $row['IDTechnologie'] . "' style='background-image: url('" . $row['URL_image'] . "');'>";
-							echo $row['libelleTechno'] . " : " . $row['brancheFinale']  ; 
-						echo "</div>";*/
-					echo "</a></td>";
+					echo "<td><a data-fancybox data-type='iframe' data-src='techno.php?id_techno=" . $row['IDTechnologie'] . "' href='javascript:;'><div class='col-sm-2 finale zoom tuile'>";
+						echo "<img src='" . $row['URL_image'] . "'  id='" . $row['IDTechnologie'] . "'   >";
+					echo "</div></a></td>";
 				}
 				else{
-					echo "<td><div class='col-sm-2 zoom tuile' id='" . $row['IDTechnologie'] . "' style='background-color: #F7CA18;' onclick='get_technos(" . $row['IDTechnologie'] . ");set_arbo(" . $row['IDTechnologie'] . ");'>";
-						echo $row['libelleTechno'] . " : " . $row['brancheFinale']  ; 
+					echo "<td><div class='col-sm-2 zoom tuile' id='" . $row['IDTechnologie'] . "' style='background-color: " . $colors[rand(0, 5)] . ";' onclick='get_technos(" . $row['IDTechnologie'] . ");set_arbo(" . $row['IDTechnologie'] . ");'>";
+						echo $row['libelleTechno'] ; 
 					echo "</div></td>";
 				}
 	}
